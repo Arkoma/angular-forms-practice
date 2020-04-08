3f38e137709afb1a38e15722c0979921964506a1
246002 <Ant0m0z!3>
2020-04-08 13:42:30 -0500

    more binding and radio buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,35 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   // reactive design
-  @ViewChild('f') signupForm: NgForm;
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+  defaultDropdown = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  defaultGender = 'non-binary';
+  user = {
+    username: '',
+    mail: '',
+    secret: {
+      question: '',
+      answer: '',
+    },
+    gender: ''
+  }
+  isFormSubmitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // console.log(Object.keys(this.signupForm.value))
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+    this.signupForm.form.patchValue({userData: { username: suggestedName}})
   }
 
   // template driven design
@@ -20,7 +45,20 @@ export class AppComponent {
   //   console.log(form);
   // }
 
+  // reactive design approach
+  // onSubmit() {
+  //   console.log(this.signupForm);
+  // }
+
   onSubmit() {
-    console.log(this.signupForm);
+    this.isFormSubmitted = true;
+
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.mail = this.signupForm.value.userData.email;
+    this.user.secret.question =  this.signupForm.value.secret;
+    this.user.secret.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
